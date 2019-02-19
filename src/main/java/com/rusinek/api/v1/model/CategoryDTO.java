12a35908af3535191e5ac497722f67b36dcc20f6
AdRusinek <rusinekadrian19@gmail.com>
2019-02-19 23:53:32 +0100

    Added first data model etc...

---
+++ src/main/java/com/rusinek/api/v1/model/CategoryDTO.java
@@ -0,0 +1,7 @@
+package com.rusinek.api.v1.model;
+
+public class CategoryDTO {
+
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/com/rusinek/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.rusinek.repositories;
+
+import com.rusinek.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
