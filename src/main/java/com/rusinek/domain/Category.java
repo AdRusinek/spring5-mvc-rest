b2281510750dad0fc06f33f29bf858f514f4e8f5
AdRusinek <rusinekadrian19@gmail.com>
2019-02-20 00:03:17 +0100

    Added bootstrap class

--- src/main/java/com/rusinek/domain/Category.java
+++ src/main/java/com/rusinek/domain/Category.java
@@ -1,11 +1,12 @@
 package com.rusinek.domain;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
+
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Entity
